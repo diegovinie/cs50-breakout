f39460854ce79628b4d8fe54b52bbd12d17c2c8e
Diego Viniegra <diego.viniegra@gmail.com>
2023-04-09 20:36:24 -0500

    Add love.js support

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var options = {
+    setHeaders: (res, path, stat) => {
+        res.set('Cross-Origin-Embedder-Policy', 'require-corp')
+        res.set('Cross-Origin-Opener-Policy', 'same-origin')
+    }
+};
+
+//setting middleware
+app.use(express.static('./dist', options));
+
+
+var server = app.listen(5000);
+
