12689a0b0f825cd26cf808aef7b4796b704bf59c
Diego Viniegra <diego.viniegra@gmail.com>
2023-04-13 12:45:13 -0500

    Look thorugh dirs

--- filepreparation.js
+++ filepreparation.js
@@ -3,10 +3,13 @@ const sep = require('path').sep;
 
 const resoucesExts = ['png','ttf', 'wav'];
 const basedir = '.' + sep;
+const depsdir = basedir + 'deps' + sep;
 const ext = '.lua';
 const inputFile =  basedir + 'main.lua';
 const outputFile = basedir + 'pathsToShip';
 const gPaths = [];
+
+let gLuaPaths = [];
 
 function onModuleFound(path) {
     gPaths.push(path);
@@ -21,11 +24,58 @@ function searchModules(text) {
 
     if (results) {
         for (const match of results) {
-            const path = basedir + match[1] + ext;
+            const module = match[1];
+            const path = basedir + module + ext;
 
-            onModuleFound(path);
+            if (fs.existsSync(path)) {
+                onModuleFound(path);
+            } else {
+                const externalPath = searchDeepModules(module);
+
+                if (externalPath) {
+                    const depsModulePath = depsdir + parseModuleToPath(module) + ext;
+
+                    prepareDirectories(depsModulePath);
+
+                    fs.copyFileSync(externalPath, depsModulePath);
+
+                    onModuleFound(depsModulePath);
+                }
+                else
+                    throw new Error(`Module ${module} not found.`);
+            }
         }
     }
+}
+
+function searchDeepModules(module) {
+    const filePath = parseModuleToPath(module);
+
+    for (const path of gLuaPaths) {
+        const fullPath = path + filePath + ext;
+
+        if (fs.existsSync(fullPath)) {
+            return fullPath;
+        }
+    }
+}
+
+function loadLuaPaths() {
+    const vars = ['LUA_PATH'];
+
+    for (const v of vars) {
+        const str = process.env[v];
+
+        if (str) gLuaPaths.push(...parseLuaPaths(str));
+    }
+}
+
+function parseLuaPaths(str) {
+    return str.replaceAll("'", '').split(';').map(line => {
+        const matches = line.match(/^(.+)\?/);
+
+        return matches[1];
+    })
 }
 
 function matchModule(text) {
@@ -50,18 +100,45 @@ function matchResource(text) {
     })
 }
 
+function parsePathToModule(path) {
+    return path.replaceAll('/', '.');
+}
+
+function parseModuleToPath(module) {
+    return module.replaceAll('.', '/');
+}
+
+function prepareDirectories(path) {
+    let str = '.' + sep;
+
+    for (const dir of path.split(sep).slice(0, -1)) {
+        if (!fs.existsSync(str + dir))
+            fs.mkdirSync(str + dir)
+
+        str += dir + sep;
+    }
+}
+
 function filterReps(items) {
     return Array.from(new Set(items));
 }
 
-function printPaths(paths) {
+function savePathsToFile(paths) {
     const content = paths.join('\n');
     fs.writeFileSync(outputFile, content);
 }
 
+
+/////////////////////////////////////////////////////////////
+
 function main(path) {
+    loadLuaPaths();
+
+    if (fs.existsSync(depsdir))
+        fs.rmSync(depsdir, { recursive: true });
+
     onModuleFound(path);
-    printPaths(filterReps(gPaths));
+    savePathsToFile(filterReps(gPaths));
 }
 
 main(inputFile);
